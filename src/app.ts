2ee1503f6d2d7a18001d16cd316ea4312f3f40a1
W. Guilherme <ravok123@gmail.com>
2021-07-01 18:46:54 -0300

    [add] helmet and morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
 import dotenv from 'dotenv'
 import routes from './router'
 
 dotenv.config();
 
 const app = express()
-app.use(cors());
 
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
 app.use(express.json())
+
 app.use(routes)
 
 //routes
