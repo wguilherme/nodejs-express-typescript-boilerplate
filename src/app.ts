2fb8dc27e52cae121e5db2c2ede88ce0e09443d8
W. Guilherme <ravok123@gmail.com>
2021-10-01 13:14:41 -0300

    [security] add rate limiter + lint npm script

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+
 import cors from 'cors'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import rateLimit from "express-rate-limit"
+
 import dotenv from 'dotenv'
 import routes from './router'
 
@@ -9,12 +12,25 @@ import routes from './router'
 
 dotenv.config();
 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+
 const app = express()
+
+app.use(express.json())
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny'));
-app.use(express.json())
+// applying into all requests
+app.use(limiter);
 
 app.use(routes)
 
